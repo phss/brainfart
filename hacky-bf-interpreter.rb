d85328742b8eb116a303aa40d303107df14373b8
Paulo Schneider <paulo.schneider@gmail.com>
2015-03-21 11:44:11 +0000

    Replace each by while, with index

--- hacky-bf-interpreter.rb
+++ hacky-bf-interpreter.rb
@@ -2,8 +2,12 @@
 def interpret(code)
   memory = Array.new(100, 0)
   pointer = 0
+  operation_index = 0
 
-  code.each_char do |operation|
+  while operation_index <= code.size
+    operation = code[operation_index]
+    operation_index += 1
+
     if operation == '>'
       pointer += 1
     elsif operation == '<'
@@ -16,6 +20,10 @@ def interpret(code)
       puts memory[pointer]
     elsif operation == ','
       # Ignore input operation
+    elsif operation == '['
+      # begin loop
+    elsif operation == ']'
+      # end loop
     end
   end
 end
