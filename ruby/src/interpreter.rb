153f9108c16f8fb36b07c4f76c8226956441e518
Paulo Schneider <paulo.schneider@gmail.com>
2015-03-22 14:36:58 +0000

    Move rules to a map

--- ruby/src/interpreter.rb
+++ ruby/src/interpreter.rb
@@ -10,33 +10,27 @@ class Interpreter
   def interpret(instructions)
     tape_controller = TapeController.new(Tape.new, 0)
     instructions_iterator = InstructionsIterator.new(instructions)
+    rules = interpretation_rules(tape_controller, instructions_iterator)
 
     while instructions_iterator.has_next?
       instruction = instructions_iterator.next
-
-      if instruction == :move_right
-        tape_controller.move_right
-      elsif instruction == :move_left
-        tape_controller.move_left
-      elsif instruction == :increment_value
-        tape_controller.increment_value
-      elsif instruction == :decrement_value
-        tape_controller.decrement_value
-      elsif instruction == :output
-        @output.print tape_controller.read.chr
-      elsif instruction == :input
-        #puts "Only numeric input supported: "
-        #tape[pointer] = gets.to_i
-      elsif instruction == :jump_past
-        if tape_controller.at_zero_cell?
-          instructions_iterator.jump_past_matching_loop
-        end
-      elsif instruction == :jump_back
-        if !tape_controller.at_zero_cell?
-          instructions_iterator.jump_back_loop_start
-        end
-      end
+      rules[instruction].call
     end
+  end
+
+ private 
+
+  def interpretation_rules(tape_controller, instructions_iterator)
+    {
+      move_right:  lambda { tape_controller.move_right },
+      move_left:  lambda { tape_controller.move_left },
+      increment_value: lambda { tape_controller.increment_value },
+      decrement_value: lambda { tape_controller.decrement_value },
+      output: lambda { @output.print tape_controller.read.chr },
+      input: lambda { },
+      jump_past: lambda { instructions_iterator.jump_past_matching_loop if tape_controller.at_zero_cell? },
+      jump_back: lambda { instructions_iterator.jump_back_loop_start unless tape_controller.at_zero_cell? },
+    }
   end
 
 end
